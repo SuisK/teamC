b6d2cc66b9473efc933b1e1275c567456e10cf27
takumi <y21503@gmail.com>
2021-12-22 13:48:51 +0900

    main.js タイマー部分

---
+++ main.js
@@ -0,0 +1,54 @@
+let start = document.getElementById("button")
+
+let startTime;
+
+let elapsedTime = 0;//経過時間をいれる
+
+let timerId;　
+
+let timeToadd = 0;//ストップから再開の時、値保持
+
+
+function updateTimeText(){
+     //分のカウント
+    let m = Math.floor(elapsedTime / 60000);
+
+    //秒のカウント
+    let s = Math.floor(elapsedTime % 60000 / 1000);
+
+    //ミリ秒のカウント
+    let ms = elapsedTime % 1000;
+
+    m = ('0' + m).slice(-2);
+    s = ('0' + s).slice(-2);
+    ms = ('0' + ms).slice(-2);
+
+    timerId.textCountent = m + ':' + s + ':' + ms;
+}
+
+function countUp(){
+    timerId = setTimeout(function(){
+
+        elapsedTime = Date.now() - startTime + timeToadd;
+        updateTimeText()
+        countUp();
+    },10);
+}
+
+
+start.addEventListener("click",function(){
+    startTime = Date.now();    //データをいれる
+    countUp();
+});
+
+
+
+
+
+
+
+
+
+
+
+//let timer = document.getElementById("timer");
