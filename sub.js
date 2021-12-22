780127d39723afa431bf776707560ffc89b7f786
SuisK <y21510@iwate-it.ac.jp>
2021-12-22 14:09:13 +0900

    Create sub.js

---
+++ sub.js
@@ -0,0 +1,5 @@
+var button = document.getElementById('button');
+var button2 = document.getElementById('button2');
+button.addEventListener("click", function() {
+    document.getElementById('button2').style.visibility = 'visible';
+})
