a08878593d7ab6c73ca90d24f2ac3402d3da39f1
SuisK <y21510@iwate-it.ac.jp>
2021-12-22 15:52:29 +0900

    Update sub.js

--- sub.js
+++ sub.js
@@ -1,11 +1,10 @@
 var button = document.getElementById('button');
 var button2 = document.getElementById('button2');
-var button3 = document.getElementById('button3');
 button.addEventListener("click", function() {
-    document.getElementById('button2').style.visibility = 'visible';
-    document.getElementById('button').style.visibility = 'hidden';
+    button2.style.visibility = 'visible';
+    button.style.visibility = 'hidden';
 });
 button2.addEventListener("click", function() {
-    document.getElementById('button3').style.visibility = 'visible';
-    document.getElementById('button2').style.visibility = 'hidden';
+    button3.style.visibility = 'visible';
+    button2.style.visibility = 'hidden';
 });
