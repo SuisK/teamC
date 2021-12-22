d4845407e939709b944b8d1a02cc403c35e8a499
SuisK <y21510@iwate-it.ac.jp>
2021-12-22 14:53:31 +0900

    Update sub.js

--- sub.js
+++ sub.js
@@ -2,4 +2,5 @@ var button = document.getElementById('button');
 var button2 = document.getElementById('button2');
 button.addEventListener("click", function() {
     document.getElementById('button2').style.visibility = 'visible';
-})
+    document.getElementById('button').style.visibility = 'hidden';
+});
